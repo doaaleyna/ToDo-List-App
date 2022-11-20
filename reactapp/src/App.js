edec008f3bd824d522f989d3b7f6da4876098a46
= <=>
2022-11-20 13:24:28 +0300

    loginform update

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,47 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
 import LoginForm from './components/loginForm';
 import ToDoList from './components/todolistPage';
 
+
 function App() {
+
+  const adminUser = {
+    email: "admin@admin.com",
+    password:"admin123"
+  }
+
+  const [user,setUser] = useState({email:""});
+  const [error,setError] = useState(""); //Hataları yakalamak için
+
+  const Login = details => {
+     console.log(details);
+
+     if(details.email == adminUser.email && details.password == adminUser.password){
+        console.log("Logged in");
+        setUser({
+          email:details.email
+        })
+     }else{
+      setError("Details do not match!");
+     }
+  }
+
+  const Logout = () => {
+     setUser({email:""})
+  }
+   
   return (
     <div className="page">
-        {/* <LoginForm/> */}
-        <ToDoList/>
+        {(user.email != "") ? (
+          <div className='welcome'>
+            <ToDoList/>
+            {/* <h2>Welcome,<span>{user.email}</span></h2> */}
+            {/* <button onClick={Logout}>Logout</button> */}
+          </div>
+        ):(
+          <LoginForm Login={Login} error={error}/>
+        )}
     </div>
   );
 }
--- reactapp/src/components/loginForm.js
+++ reactapp/src/components/loginForm.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import "./loginForm.css"
 
-const LoginForm = () => {
-    return (
-         <div className="cover">
-            <h1>Login</h1>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+function LoginForm({Login,error}) {
 
-            <div className="login-btn">Login</div>
-            
-         </div>
+    const [details,setDetails] = useState({email:"",password:""});
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        Login(details);
+    }
+
+    return (
+         <form className="cover" onSubmit={submitHandler}>
+            <h1>Login</h1>
+            {(error != "") ? (<div className="error">{error}</div>): ""}
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" onChange={e => setDetails({...details,email:e.target.value})} value={details.email}/>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" onChange={e => setDetails({...details,password:e.target.value})} value={details.password}/>
+            <button className="login-btn" type="submit">Login</button>
+         </form>
     )
 }
 
-export default LoginForm
+export default LoginForm;
